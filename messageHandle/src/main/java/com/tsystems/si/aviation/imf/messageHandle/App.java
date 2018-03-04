05a12c2fcad056c1e6096d4329d3979169102c1c
Bolo_fang <bfang@DCNA9133.pc.cn.t-internal.com>
2018-03-04 16:01:33 +0800

    update new dependecy

--- messageHandle/src/main/java/com/tsystems/si/aviation/imf/messageHandle/App.java
+++ messageHandle/src/main/java/com/tsystems/si/aviation/imf/messageHandle/App.java
@@ -1,5 +1,7 @@
 package com.tsystems.si.aviation.imf.messageHandle;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -9,6 +11,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class App 
 {
+	private static final Logger     logger               = LoggerFactory.getLogger(App.class);
     public static void main( String[] args )
     {
     	ApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"applicationContext.xml"});
